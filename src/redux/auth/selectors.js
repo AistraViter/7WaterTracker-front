79b3ea7795b80066dc9a65961037278a14cd3f8e
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-18 04:38:51 +0300

    заготовка

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,4 +1,1 @@
-export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
-export const selectIsRefreshing = (state) => state.auth.isRefreshing;
-export const selectUser = (state) => state.auth.user;
 
