0feb149079f2fbcca915222d307c1f690ec7cced
Yelyzaveta756 <selanta19@gmail.com>
2024-10-18 17:30:58 +0300

    додала папки всіх компонентів  та сторінок проекту + додала шрифти

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+// для НЕ авторизованого користувача вставляє компоненти Logo та UserAuth(посилання на signupPage)
+// для авторизованого користувача вставляє компоненти Logo та UserLogo (має містити компонент 
+// UserLogoModal -> містить SettingModal, UserLogoutModal)
+
+//можна в Header вставляти окремі компоненти, наприклад з Logo та UserLogo зробити компонент UserMenu
+
+//Header як appBar в попередніх дз
