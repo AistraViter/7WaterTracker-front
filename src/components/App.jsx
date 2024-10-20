ee1de33cf443cbaba8a217b28881edd4bfa68d5c
Denis9622 <152106321+Denis9622@users.noreply.github.com>
2024-10-20 15:03:33 +0300

    /signin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ import { SharedLayout } from "../components/SharedLayout/SharedLayout.jsx"
 import css from "./App.module.css"
 
 const HomePage = lazy(()=> import ("../pages/HomePage/HomePage.jsx"))
-const SignUpPage = lazy(()=> import ("../pages/SignupPage/SignupPage.jsx"))
+const SignUpPage = lazy(() => import("../pages/SignupPage/SignupPage.jsx"))
+const SigninPage = lazy(() => import("../pages/SigninPage/SigninPage.jsx"));  
 {/* писати маршрути нижче */}
 
 const NotFoundPage = lazy(() => import ("../pages/NotFoundPage/NotFoundPage.jsx"))
@@ -15,18 +16,18 @@ const NotFoundPage = lazy(() => import ("../pages/NotFoundPage/NotFoundPage.jsx"
 export default function App() {
 
   return (
-      <div className={css.app}>
-        {/* замінимо на Loader пізніше  */}
-        <Suspense fallback={<div>Loading...</div>}> 
+    <div className={css.app}>
+      {/* замінимо на Loader пізніше  */}
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes path="/" element={<SharedLayout />}>
-            <Route path="/home" element={<HomePage />}></Route>
-            <Route path="/signup" element={<SignUpPage />}></Route>
-            {/* писати маршрути нижче */}
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/signup" element={<SignUpPage />}></Route>
+          <Route path="/signin" element={<SigninPage />}></Route>
+          {/* писати маршрути нижче */}
 
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-          </Suspense>
-        
-      </div>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </div>
   );
 }
