90a11638bd2b47a5c316875b2dfe5cb11e117680
Samolyb <artemte6575@gmail.com>
2024-10-20 19:15:29 +0100

    Update-app-header-sharedlayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn, selectUser } from "../../redux/auth/slice";
+import { selectIsLoggedIn, selectUser } from "../../redux/authSlice";
 
 export const SharedLayout = () => {
   const isAuthenticated = useSelector(selectIsLoggedIn);
