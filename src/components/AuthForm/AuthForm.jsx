5adb50e387f214a14b04e6a936967a5ced7c3d75
radyvoniuk <radyvoniukkate@gmail.com>
2024-10-21 00:11:35 +0300

    SignUp page and AuthForm code

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,47 +1,102 @@
 // цей компонент бути як і на сторінці реєстрації та і на сторінці логіну (немає відокремлення)
 
 import { useDispatch } from "react-redux"
-import { signup } from "../../redux/auth/operations.js";
+import { signup, signin } from "../../redux/auth/operations.js";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import css from './AuthForm.module.css'
 
-export default function RegistrationForm() {
+export default function AuthForm({ isSignUp, onSuccess }) {
+  const initialValues = {
+    email: "",
+    password: "",
+    repeatPassword: isSignUp ? "" : "",
+  };
 
-    const initialValues = {
-        name: '',
-        email: '',
-        password: '',
-      };
+  const dispatch = useDispatch();
 
-      const dispatch = useDispatch()
-
-    const handleSubmit = (values) => {
-        console.log("Submitting values:", values);
-        dispatch(signup(values))
-        .then(response => {
-            console.log("Registration successful:", response);
+  const handleSubmit = (values) => {
+    if (isSignUp) {
+      if (values.password !== values.repeatPassword) {
+        console.error("Passwords do not match");
+        return;
+      }
+      dispatch(signup(values))
+        .then((response) => {
+          console.log("Registration successful:", response);
+          if (onSuccess) onSuccess();
         })
-        .catch(error => {
-            console.error("Registration failed:", error);
+        .catch((error) => {
+          console.error("Registration failed:", error);
+        });
+    } else {
+      dispatch(signin(values))
+        .then((response) => {
+          console.log("Login successful:", response);
+          if (onSuccess) onSuccess();
         })
+        .catch((error) => {
+          console.error("Login failed:", error);
+        });
     }
+  };
 
-return (
-    <div className={css.registrationContainer}>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            <Form className={css.form}>
-                <label htmlFor="name" className={css.labelfirst}>Name</label>
-                <Field type="text" name="name" autoComplete="off" className={css.input} required></Field>
-                <ErrorMessage name='name' component='span' className={css.error} />
-                <label htmlFor="email" className={css.labelfirst}>Email</label>
-                <Field type="email" name="email" autoComplete="off" className={css.input} required></Field>
-                <ErrorMessage name='email' component='span' className={css.error} />
-                <label htmlFor="password" className={css.labelfirst}>Password</label>
-                <Field type="password" name="password" autoComplete="off" className={css.input} required></Field>
-                <ErrorMessage name='password' component='span' className={css.error} />
-                <button type="submit" className={css.addBtn}>Register</button>
-            </Form>
-        </Formik>
+  return (
+    <div className={css.AuthContainer}>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={css.form}>
+          <label htmlFor="email" className={css.labelfirst}>
+            Enter your email
+          </label>
+          <Field
+            type="email"
+            name="email"
+            autoComplete="off"
+            className={css.input}
+            placeholder="E-mail"
+            required
+          ></Field>
+          <ErrorMessage name="email" component="span" className={css.error} />
+          <label htmlFor="password" className={css.labelfirst}>
+            Enter your password
+          </label>
+          <Field
+            type="password"
+            name="password"
+            autoComplete="off"
+            className={css.input}
+            placeholder="Password"
+            required
+          ></Field>
+          <ErrorMessage
+            name="password"
+            component="span"
+            className={css.error}
+          />
+          {isSignUp && (
+            <>
+              <label htmlFor="repeatPassword" className={css.labelfirst}>
+                Repeat Password
+              </label>
+              <Field
+                type="password"
+                name="repeatPassword"
+                autoComplete="off"
+                className={css.input}
+                placeholder="Repeat password"
+                required
+              />
+              <ErrorMessage
+                name="repeatPassword"
+                component="span"
+                className={css.error}
+              />
+            </>
+          )}
+          <button type="submit" className={css.addBtn}>
+            {isSignUp ? "Sign Up" : "Login"}
+          </button>
+        </Form>
+      </Formik>
     </div>
-)
+  );
 }
