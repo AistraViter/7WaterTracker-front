d3ad2449d1d6f5e594be5beea31ca221e1725389
RomanBabych <rrroma.babi42017@gmail.com>
2024-10-23 22:42:44 +0300

    change logout button logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,17 +6,20 @@
 
 //Header як appBar в попередніх дз
 
-import { useSelector } from "react-redux";
-import { selectUser, selectIsLoggedIn } from "../../redux/auth/selectors";
 import Logo from "../Logo/Logo";
 import UserAuth from "../UserAuth/UserAuth";
 import UserLogo from "../UserLogo/UserLogo";
 import css from "../Header/Header.module.css";
 
 export default function Header() {
-  const isAuthenticated = useSelector(selectIsLoggedIn);
-  const user = useSelector(selectUser);
-
+  const isAuthenticated = true;
+  const user = {
+    name: "Ivan",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4YreOWfDX3kK-QLAbAL4ufCPc84ol2MA8Xg&s",
+    email: "vasya@228gmail.com",
+  };
+  console.log(isAuthenticated, "isAuthenticated");
   return (
     <header className={css.header}>
       <Logo />
--- src/components/Modal/UserLogoModal/UserLogoModal.jsx
+++ src/components/Modal/UserLogoModal/UserLogoModal.jsx
@@ -1,31 +1,32 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { HiCog6Tooth } from "react-icons/hi2";
 import { HiOutlineLogout } from "react-icons/hi";
 import css from "./UserLogoModal.module.css";
+// import UserLogoutModal from "../UserLogoutModal/UserLogoutModal";
 
-const UserLogoModal = ({ closeModal }) => {
+const UserLogoModal = () => {
   const navigate = useNavigate();
   const modalRef = useRef(null);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
   const navigateToPath = (path) => {
     navigate(`/${path}`);
   };
 
-  const handleLogout = () => {
-    localStorage.removeItem("accessToken");
-    navigateToPath("signin");
+  const handleLogoutClick = () => {
+    setIsLogoutModalOpen(true); // Открываем модальное окно для подтверждения логаута
   };
 
   const handleClickOutside = (event) => {
     if (modalRef.current && !modalRef.current.contains(event.target)) {
-      closeModal();
+      setIsLogoutModalOpen(false); // Закрываем модальное окно, если клик вне его (опционально)
     }
   };
 
   const handleKeyDown = (event) => {
     if (event.key === "Escape") {
-      closeModal();
+      setIsLogoutModalOpen(false);
     }
   };
 
@@ -40,19 +41,26 @@ const UserLogoModal = ({ closeModal }) => {
   }, []);
 
   return (
-    <div className={css.dropdownMenu} ref={modalRef}>
-      <button
-        className={css.settingsBtn}
-        onClick={() => navigateToPath("settings")}
-      >
-        <HiCog6Tooth className={css.settingsIcon} />
-        Settings
-      </button>
-      <button className={css.logoutBtn} onClick={handleLogout}>
-        <HiOutlineLogout className={css.logoutIcon} />
-        Log out
-      </button>
-    </div>
+    <>
+      <div className={css.dropdownMenu} ref={modalRef}>
+        <button
+          className={css.settingsBtn}
+          onClick={() => navigateToPath("settings")}
+        >
+          <HiCog6Tooth className={css.settingsIcon} />
+          Settings
+        </button>
+        <button className={css.logoutBtn} onClick={handleLogoutClick}>
+          <HiOutlineLogout className={css.logoutIcon} />
+          Log out
+        </button>
+      </div>
+      {isLogoutModalOpen && (
+        <div>
+          Коли модалка логаута буде працювати - замінити на UserLogoutModal
+        </div>
+      )}
+    </>
   );
 };
 
