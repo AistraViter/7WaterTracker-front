3adcda6a584de3899f866d99275ce6d44c7694d3
RomanBabych <rrroma.babi42017@gmail.com>
2024-10-23 21:29:12 +0300

    markup done, part of logic done

--- src/components/Modal/UserLogoModal/UserLogoModal.jsx
+++ src/components/Modal/UserLogoModal/UserLogoModal.jsx
@@ -1,11 +1,59 @@
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { HiCog6Tooth } from "react-icons/hi2";
+import { HiOutlineLogout } from "react-icons/hi";
+import css from "./UserLogoModal.module.css";
 
-const UserLogoModal = ({ onLogout, onSettings }) => {
-    return (
-        <div className="modal">
-            <button onClick={onSettings}>Settings</button>
-            <button onClick={onLogout}>Log out</button>
-        </div>
-    );
+const UserLogoModal = ({ closeModal }) => {
+  const navigate = useNavigate();
+  const modalRef = useRef(null);
+
+  const navigateToPath = (path) => {
+    navigate(`/${path}`);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    navigateToPath("signin");
+  };
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      closeModal();
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Escape") {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <div className={css.dropdownMenu} ref={modalRef}>
+      <button
+        className={css.settingsBtn}
+        onClick={() => navigateToPath("settings")}
+      >
+        <HiCog6Tooth className={css.settingsIcon} />
+        Settings
+      </button>
+      <button className={css.logoutBtn} onClick={handleLogout}>
+        <HiOutlineLogout className={css.logoutIcon} />
+        Log out
+      </button>
+    </div>
+  );
 };
 
 export default UserLogoModal;
--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,30 +1,34 @@
-import { useState } from 'react';
+import { useMemo, useState } from "react";
 import UserLogoModal from "../Modal/UserLogoModal/UserLogoModal";
+import { HiOutlineChevronDown } from "react-icons/hi2";
+import css from "./UserLogo.module.css";
 
-export default function UserLogo({ user, handleLogout }) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+export default function UserLogo({ user }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const toggleModal = () => setIsModalOpen(!isModalOpen);
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
+  const closeModal = () => setIsModalOpen(false);
 
-    const handleSettings = () => {
-        // console.log("Settings");
-    };
+  const initials = useMemo(
+    () =>
+      user?.name ? user.name[0].toUpperCase() : user.email[0].toUpperCase(),
+    [user.name, user.email]
+  );
 
-    return (
-        <div className="user-logo" onClick={toggleModal}>
-            <img
-                src={user.avatar || "user-avatar"}
-                alt={user.name || user.email}
-            />
-            <span>{user.name || user.email}</span>
-
-            {isModalOpen && (
-                <UserLogoModal
-                    onLogout={handleLogout}
-                    onSettings={handleSettings}
-                />
-            )}
+  return (
+    <>
+      <div className={css.userLogo} onClick={toggleModal}>
+        <span className={css.userName}>{user.name || user.email}</span>
+        <div className={css.avatarWrapper}>
+          {user?.avatar ? (
+            <img className={css.userAvatar} src={user.avatar} alt={initials} />
+          ) : (
+            <span className={css.initials}>{initials}</span>
+          )}
         </div>
-    );
+        <HiOutlineChevronDown className={css.dropdownIcon} />
+      </div>
+      {isModalOpen && <UserLogoModal closeModal={closeModal} />}
+    </>
+  );
 }
-
