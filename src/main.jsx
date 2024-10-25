4ef1ee82d21413026a6f743e68fbad3033196c56
uliacop <115625925+uliacop@users.noreply.github.com>
2024-10-25 20:45:52 +0300

    Update main.jsx
    delete TestApp

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,17 @@ import { PersistGate } from "redux-persist/integration/react";
 import { HelmetProvider } from "react-helmet-async";
 import { store, persistor } from "./redux/store.js";
 import App from "./components/App.jsx";
-import TestApp from "./components/TestApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <HelmetProvider>
-            {/* <App /> */}
-            <TestApp/>
-        </HelmetProvider>
-      </BrowserRouter>
-    </PersistGate>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
