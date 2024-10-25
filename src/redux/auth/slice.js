150a39873d04cd49c0122b434d5e1c1537017f1c
Yelyzaveta756 <selanta19@gmail.com>
2024-10-25 21:31:47 +0300

    change url for userLogo, add privateRoute to HomePage

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -10,7 +10,7 @@ const authSlice = createSlice({
     },
     token: null,
     isLoggedIn: false,
-    isRefreshing: false,
+    isRefreshing: false, 
     loading: false,
     error: null,
   },
@@ -22,14 +22,12 @@ const authSlice = createSlice({
         state.isLoggedIn = false;
       })
       .addCase(signup.fulfilled, (state, action) => {
-        console.log(action.payload); 
         state.user = action.payload.user;
-        state.token = action.payload.accessToken; 
+        state.token = action.payload.accessToken;
         state.isLoggedIn = true;
         state.error = null;
         state.loading = false;
       })
-
       .addCase(signup.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
@@ -52,28 +50,9 @@ const authSlice = createSlice({
         state.error = action.payload;
         state.isLoggedIn = false;
       })
-      .addCase(refresh.pending, (state) => {
+      .addCase(logout.pending, (state) => {
         state.loading = true;
-        state.isLoggedIn = false;
         state.error = null;
-        state.isRefreshing = true;
-      })
-      .addCase(refresh.fulfilled, (state, action) => {
-        state.user = action.payload
-        state.isLoggedIn = true;
-        state.isRefreshing = false;
-        state.error = null;
-        state.loading = false;
-      })
-      .addCase(refresh.rejected, (state, action) => {
-        state.isRefreshing = false;
-        state.isLoggedIn = false;
-        state.error = action.payload;
-        state.loading = false;
-      })
-      .addCase(logout.pending, (state, action) => {
-        state.loading = true;
-        state.error = action.payload;
       })
       .addCase(logout.fulfilled, (state) => {
         state.user = { password: null, email: null };
@@ -85,9 +64,21 @@ const authSlice = createSlice({
       .addCase(logout.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
+      })
+      .addCase(refresh.pending, (state) => {
+        state.isRefreshing = true; // Set refreshing state to true
+      })
+      .addCase(refresh.fulfilled, (state, action) => {
+        state.user = action.payload.user; // Update user data
+        state.token = action.payload.accessToken; // Update token if necessary
+        state.isRefreshing = false; // Reset refreshing state
+      })
+      .addCase(refresh.rejected, (state, action) => {
+        state.isRefreshing = false; // Reset refreshing state
+        state.error = action.payload; // Set the error if refresh fails
       });
-  }
-})
+  },
+});
 
 export const authReducer = authSlice.reducer;
 
