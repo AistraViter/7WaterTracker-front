3813d8a9d1de2c49166ad1ea5c059f250fd7d2ea
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-27 19:02:11 +0200

    Update TEST.jsx

--- src/components/TEST.jsx
+++ src/components/TEST.jsx
@@ -1,3 +1,23 @@
 
-
+const handleSave = async (waterVolume, time) => {
+    const formattedDate = formatTimeToDate(time);
+    
+    console.log("Water volume to be sent:", waterVolume);
+    console.log("Formatted date:", formattedDate);
+    
+    try {
+      const response = await dispatch(
+        updateWaterNote({ _id: waterId, waterVolume, date: formattedDate })
+      ).unwrap();
   
+      console.log("WaterNote successfully saved:", response);
+      setWaterData({
+        waterVolume: response.waterVolume,
+        time: response.time || waterData.time,
+      });
+      onClose(); // Закриваємо модалку після успішного збереження
+    } catch (error) {
+      console.error("Error on saving data:", error);
+    }
+  };
+    
