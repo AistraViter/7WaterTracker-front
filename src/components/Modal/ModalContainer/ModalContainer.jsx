6ca2aaae7529ea965a29f726dcb3aa3fa252e3c0
radyvoniuk <radyvoniukkate@gmail.com>
2024-10-27 12:00:58 +0200

    modal styles

--- src/components/Modal/ModalContainer/ModalContainer.jsx
+++ src/components/Modal/ModalContainer/ModalContainer.jsx
@@ -23,28 +23,48 @@ const ModalContainer = ({
     };
   }, [modalIsOpen]);
 
+  const handleOverlayClick = (event) => {
+    // Якщо клік відбувся саме на оверлеї (фоні), закриваємо модалку
+    if (event.target.classList.contains(css.backdrop)) {
+      closeModal();
+    }
+  };
+
+  const handleModalClick = (event) => {
+    event.stopPropagation(); // Блокує подальше поширення події на батьківські елементи
+  };
+
   return (
     <Modal
       isOpen={modalIsOpen}
       onRequestClose={closeModal}
-      shouldCloseOnOverlayClick={true}
+      shouldCloseOnOverlayClick={false}
       ariaHideApp={false}
       className={css.modal}
       overlayClassName={css.backdrop}
+      onClick={handleOverlayClick}
     >
-      {children}
-      <button
-        className={clsx(
-          css["close-button"],
-          buttonClassLogout && css["button-class-logout"],
-          buttonClassSettings && css["button-class-settings"]
-        )}
-        onClick={closeModal}
-      >
-        <svg className={css["close-icon"]}>
-          <use href="/public/icon.svg#icon-outline"></use>
-        </svg>
-      </button>
+      <div onClick={handleModalClick} className={css.modalContent}>
+        {children}
+        <button
+          className={clsx(
+            css["close-button"],
+            buttonClassLogout && css["button-class-logout"],
+            buttonClassSettings && css["button-class-settings"]
+          )}
+          onClick={closeModal}
+        >
+          <svg className={css.closeIcon} viewBox="0 0 32 32">
+            <path
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeMiterlimit="4"
+              strokeWidth="2"
+              d="M8 24l16-16M8 8l16 16"
+            ></path>
+          </svg>
+        </button>
+      </div>
     </Modal>
   );
 };
