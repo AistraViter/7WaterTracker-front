9f8bc6bebc0575f8a6c30eb66b793b38c60dba86
Yura Kasyan <33.yurakasyan@gmail.com>
2024-10-28 23:10:52 +0100

    fix: changes defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    target: "esnext", // Підтримка новіших функцій, включаючи top-level await
+  },
   plugins: [react()],
-})
+});
