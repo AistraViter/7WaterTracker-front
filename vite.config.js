5d0a3480df6582d672798b51192cf14c96dac8d9
Yura Kasyan <33.yurakasyan@gmail.com>
2024-10-28 23:32:52 +0100

    back

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    target: "esnext", // Підтримка новіших функцій, включаючи top-level await
-  },
   plugins: [react()],
 });
