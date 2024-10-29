dd34ca687ae7779921cd413b49b15d587cc1e805
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-29 04:15:46 +0200

    Відновлення

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,24 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import AddWaterAmountModal from "../Modal/AddWaterAmountModal/AddWaterAmountModal.jsx";
-import { useSelector } from "react-redux";
-import { selectWaterPercentage } from "../../redux/water/selectors.js";
+import { getWaterToday } from '../../redux/water/operations.js';
+import { selectWaterPercentage } from "../../redux/water/selectors.js"; // Шлях до вашого waterSlice
 
 import css from "./WaterRatioPanel.module.css";
 
 const WaterRatioPanel = () => {
+  const dispatch = useDispatch();
+  
+  // Отримання percentage з Redux-стану за допомогою селектора
+  const waterPercentage = useSelector(selectWaterPercentage);
+
+  // Виконання getWaterToday при монтуванні компонента
+  useEffect(() => {
+    dispatch(getWaterToday());
+  }, [dispatch]);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const waterPercantage = useSelector(selectWaterPercentage);
 
   const handleAddWater = () => {
     setIsModalOpen(true);
-  };
+  }; 
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
   const getMilestoneStyle = (milestoneValue) => {
-    return Math.round(waterPercantage) === milestoneValue
+    return Math.round(waterPercentage) === milestoneValue
       ? {
           fontFamily: "Roboto",
           fontSize: "16px",
@@ -26,6 +36,7 @@ const WaterRatioPanel = () => {
           lineHeight: "20px",
           textAlign: "center",
           color: "#407bff",
+          
         }
       : {
           fontFamily: "Roboto",
@@ -36,6 +47,8 @@ const WaterRatioPanel = () => {
           color: "#407bff",
         };
   };
+  useEffect(() => {
+}, [waterPercentage]);
 
   return (
     <div className={css.waterratiopanel}>
@@ -46,7 +59,7 @@ const WaterRatioPanel = () => {
           <div className={css.progressBarContainer}>
             <div
               className={css.progressBar}
-              style={{ width: `${waterPercantage}%` }}
+              style={{ width: `${waterPercentage}%` }}
             ></div>
 
             <div className={css.milestones}>
@@ -54,7 +67,7 @@ const WaterRatioPanel = () => {
                 <span className={css.line}>|</span> 0%
               </div>
               <div className={css.milestone} style={getMilestoneStyle(50)}>
-                <span className={css.line}>|</span> 50%
+                <span className={css.line} >|</span > {50}%
               </div>
               <div className={css.milestone} style={getMilestoneStyle(100)}>
                 <span className={css.line}>|</span> 100%
@@ -63,10 +76,10 @@ const WaterRatioPanel = () => {
 
             <div
               className={css.circle}
-              style={{ left: `${waterPercantage}%` }}
+              style={{ left: `${waterPercentage}%` }}
             ></div>
           </div>
-        </div>{" "}
+        </div>
         {/* Закриваючий тег для блоку data */}
         <button onClick={handleAddWater} className={css.btn}>
           <svg width="24" height="24" className={css.plusicon}>
